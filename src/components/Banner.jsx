db9ca2b48d99e6b17f7557c0b4487b69b66bcf2f
garden1204 <a01062374285@gmail.com>
2019-11-19 19:01:21 +0900

    net

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,71 +1,81 @@
 import React from "react";
 import styled from "styled-components";
 
-const Logo = styled.img`
-   height:100%;
-   margin: 10px 0;
-
+const Wrapper = styled.div`
+  width: 100vw;
+  height: 68px;
+  display: flex;
+  padding: 20px 58px;
+  box-sizing: border-box;
 `;
 
-const Wrapper = styled.div`
-    width:100vw;
-    height:68px;
-    display:flex;
-    padding:20px 58px;
-    box-sizeing:border-box;
+const Logo = styled.img`
+  height: 100%;
+  margin-right: 30px;
 `;
 
 const NavText = styled.ul`
-    display:flex;
-    color:white;
-    list-style:none;
-    height:100%;
-    flex:1;
-    align-items:center;
-`;
-
-const NavIcon = styled.ul`
-    display:flex;
-    list-style:none;
-    height:100%;
-   
+  display: flex;
+  color: white;
+  list-style: none;
+  height: 100%;
+  flex: 1;
+  align-items: center;
 `;
 
 const NavTextItem = styled.li`
-    
+  margin-right: 20px;
+  font-weight:normal;
+  color:#e5e5e5;
+  :hover{
+      color:#b3b3b3;
+      font-weight:normal;
+  }
+  :active{
+      font-weight:bold;
+      color:#000;
+  }
+`;
+
+const NavIcon = styled.ul`
+  display: flex;
+  list-style: none;
+  height: 100%;
+  
 `;
 
 const NavIconItem = styled.li`
-    width:fit-content;
-
-     & > img{
-        height:100%;
-    }
+  width: fit-content;
+  margin-right: 60px;
+  & > img {
+    height: 100%;
+  }
 `;
 
-
-
-
 const Banner = props => {
-    return (
+  return (
     <Wrapper>
-        <Logo src="https://cdn.pixelprivacy.com/wp-content/uploads/2017/12/Netflix-Logo-1024x277.png" alt=""/>
-        <NavText>
-            <NavTextItem>홈</NavTextItem>
-            <NavTextItem>TV 프로그램</NavTextItem>
-            <NavTextItem>영화</NavTextItem>
-            <NavTextItem>최신 등록 콘텐츠</NavTextItem>
-            <NavTextItem>내가 찜한 콘텐츠</NavTextItem>
-            </NavText>
-            <NavIcon>
-                <NavIconItem><img src="https://st3.depositphotos.com/5532432/17958/v/1600/depositphotos_179582526-stock-illustration-magnifier-flat-vector-icon.jpg" alt=""/></NavIconItem>
-                <NavIconItem><img src="" alt=""/></NavIconItem>
-                <NavIconItem><img src="" alt=""/></NavIconItem>
-            </NavIcon>
+      <Logo src="https://cdn.pixelprivacy.com/wp-content/uploads/2017/12/Netflix-Logo-1024x277.png" />
+      <NavText>
+        <NavTextItem>홈</NavTextItem>
+        <NavTextItem>TV 프로그램</NavTextItem>
+        <NavTextItem>영화</NavTextItem>
+        <NavTextItem>최신 등록 콘텐츠</NavTextItem>
+        <NavTextItem>내가 찜한 콘텐츠</NavTextItem>
+      </NavText>
+      <NavIcon>
+        <NavIconItem>
+          <img src="/search.svg" alt="" />
+        </NavIconItem>
+        <NavIconItem>
+          <img src="/gift.svg" alt="" />
+        </NavIconItem>
+        <NavIconItem>
+          <img src="/bell.svg" alt="" />
+        </NavIconItem>
+      </NavIcon>
     </Wrapper>
-    );
+  );
 };
-
-
 
 export default Banner;
